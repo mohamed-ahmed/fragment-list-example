d078ee55a574ca4c37a9cbc42e9be03086a7164c
Mohamed Ahmed <mohamed.ahmed@live.ca>
2015-08-05 18:28:54 -0400

    binded correct layout to inflater

--- FragmentListExample/app/src/main/java/com/example/mohamedahmed/fragmentlistexample/MainActivityFragment.java
+++ FragmentListExample/app/src/main/java/com/example/mohamedahmed/fragmentlistexample/MainActivityFragment.java
@@ -18,6 +18,6 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        return inflater.inflate(R.layout.fragment_container_layout, container, false);
     }
 }
